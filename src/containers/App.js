2a2507f431b5b8e8afd92f7f28e15d1936b0e966
AleksejDonets <brut94@gmail.com>
2019-07-04 19:25:59 +0300

    Додав material-ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,17 +10,10 @@ class App extends Component  {
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-          </header>
-        </div>
       </Router>
     )
   }
   
 }
 
-export default App;
+export default connect()(App);
